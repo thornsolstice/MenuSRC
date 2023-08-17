b5394f9a82d30a07fb6886d583e7191b4b19fa1b
solstice <141642047+thornsolstice@users.noreply.github.com>
2023-08-17 14:09:14 +0100

    Add files via upload

---
+++ MenuSRC.py
@@ -0,0 +1,53 @@
+# MENU BETA
+# Developed by blazetapped
+# Open Source
+# Development begun - 17/08/2023 -- ??/??/???? #
+################################################################
+# Config
+import time
+import typing
+################################################################
+# Main Menu
+print("Loading..")
+time.sleep(1)
+print("Loaded!")
+time.sleep(0.5)
+print("                                __  __________   ____  _______ ____  ______")     
+time.sleep(0.4)
+print("                               /  |/  / ____/ | / / / / / ___// __ \/ ____/")
+time.sleep(0.4)
+print("                              / /|_/ / __/ /  |/ / / / /\__ \/ /_/ / /    ")
+time.sleep(0.4)
+print("                             / /  / / /___/ /|  / /_/ /___/ / _, _/ /___")
+time.sleep(0.4)
+print("                            /_/  /_/_____/_/ |_/\____//____/_/ |_|\____/   ")
+time.sleep(0.4)
+print("                                     Developed by blazetapped")
+time.sleep(0.3)
+print("                                          Open Source")
+time.sleep(0.3)
+print("                                              1.0")   
+time.sleep(0.3)
+print("                                  [1] Changelog [2] Information")
+print("                                           [9] Exit")
+# All done on 17/08/2023
+################################################################
+# Interactable System #
+################################################################
+if input() :
+     print("                                             Changelog")
+     print("                                                1.0")
+     print("                                              Release")
+     print("")
+     print("                                         Press [1] To exit")
+     if input():
+      if input():
+        print("                                            Information")
+print("               MenuSRC is a small project that is worked on individually by blazetapped.")
+print("           This small project is to help me learn more things about Python and how to use it.")
+print("                I am planning on updating it whenever I can to make this look better.")
+print("")
+print("                                        Press [2] To exit")
+
+
+input()
